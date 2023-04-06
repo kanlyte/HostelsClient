d59f978cc8b219994beb710890b1f3c164a621aa
unknown <nahuriragaston@gmail.com>
2023-04-06 12:57:23 +0300

    d

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 // const url = "http://127.0.0.1:5051/api/v6";
-const url = "https://wide-eyed-boa-boot.cyclic.app/api/v6";
-// const url = "https://api.kanlyte.com";
+// const url = "https://wide-eyed-boa-boot.cyclic.app/api/v6";
+const url = "https://api.kanlyte.com";
 
 export default class FormsApi {
   //post requests
